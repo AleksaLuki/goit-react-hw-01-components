b908e4caa899869a1cb0b8d2a0cd050621cfb525
AleksaLuki <dimirtiy9000@gmail.com>
2023-04-23 11:27:34 +0300

    prof and stat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template1
+//     </div>
+//   );
+// };
+import css from './App.module.css'
+
+import { Profile } from './Profile/Profile';
+import user from './Profile/user';
+
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template1
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+   <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
